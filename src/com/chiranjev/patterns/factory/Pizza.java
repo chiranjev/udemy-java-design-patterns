b7a11d70f7ddf28ddba809008f2db0055b1a8f35
Chiranjev Koul <koulsahil04321@gmail.com>
2021-12-14 03:35:01 +0530

    code for factory pattern

---
+++ src/com/chiranjev/patterns/factory/Pizza.java
@@ -0,0 +1,7 @@
+package com.chiranjev.patterns.factory;
+
+public interface Pizza {
+    void prepare();
+    void bake();
+    void cut();
+}
---
+++ src/com/chiranjev/patterns/factory/Test.java
@@ -0,0 +1,8 @@
+package com.chiranjev.patterns.factory;
+
+public class Test {
+    public static void main(String[] args) {
+        PizzaStore ps = new PizzaStore();
+        ps.orderPizza("chicken");
+    }
+}
