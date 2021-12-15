d82a2ff3d59f46d5bd37ffce81499f17456b6851
Chiranjev Koul <koulsahil04321@gmail.com>
2021-12-15 08:40:23 +0530

    code for command pattern

---
+++ src/com/chiranjev/patterns/command/Command.java
@@ -0,0 +1,5 @@
+package com.chiranjev.patterns.command;
+
+public interface Command {
+    void execute();
+}
---
+++ src/com/chiranjev/patterns/command/OffCommand.java
@@ -0,0 +1,15 @@
+package com.chiranjev.patterns.command;
+
+public class OffCommand implements Command{
+
+    Television television;
+
+    public OffCommand(Television television){
+        this.television = television;
+    }
+
+    @Override
+    public void execute() {
+        television.off();
+    }
+}
